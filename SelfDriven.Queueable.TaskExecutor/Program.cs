b4e56f3202d921de19e935bb2117c5e660e4d9b2
Kushan Devarajegowda <kushan.devarajegowda@philips.com>
2021-11-26 19:14:01 +0530

    Add project files.

---
+++ SelfDriven.Queueable.TaskExecutor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SelfDriven.Queueable.TaskExecutor
+{
+    class Program
+    {
+        public class MyTaskRequest : ITaskRequest
+        {
+            public MyTaskRequest(string id, int delay)
+            {
+                Id = id;
+                Delay = delay;
+            }
+            public string Id { get; set; }
+            public int Delay { get; set; }
+        }
+
+        public class MyTaskExecutor : IQueueableTaskExecutor<MyTaskRequest>
+        {
+            public void Execute(MyTaskRequest request)
+            {
+                Log("Started Id '{0}', Delay{1}", request.Id, request.Delay);
+                Thread.Sleep(request.Delay);
+                Log("Finished Id '{0}'", request.Id);
+            }
+
+            private void Log(string v, params object[] args)
+            {
+                Console.WriteLine("[{0}]\t[{1}]\t[{2}]", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), Task.CurrentId, string.Format(v, args));
+            }
+
+            public void Initialize()
+            {
+                Log("Initialized Executor{0}", nameof(MyTaskExecutor));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var qte = TaskQueue<MyTaskExecutor, MyTaskRequest>.CreateInstance();
+            //First task request will kickstart the execution.
+            qte.Enqueue(new MyTaskRequest("1", 100));
+            qte.Enqueue(new MyTaskRequest("2", 200));
+            qte.Enqueue(new MyTaskRequest("3", 300));
+            Thread.Sleep(620);
+            //Tasks from this request will be executed by new task, as kickstarted by this request.
+            qte.Enqueue(new MyTaskRequest("4", 500));
+            qte.Enqueue(new MyTaskRequest("5", 400));
+            qte.Enqueue(new MyTaskRequest("6", 400));
+            qte.Enqueue(new MyTaskRequest("7", 400));
+            //Task 5&6 will be paused as per the stop request.
+            Thread.Sleep(620);
+            qte.Stop();
+            //Task 5&6 will be executed by new task, as kickstarted by this Start request.
+            qte.Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SelfDriven.Queueable.TaskExecutor/QueueableAsyncWorker.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace SelfDriven.Queueable.TaskExecutor
+{
+    public sealed class ThreadSafeBool
+    {
+        private object _locker = new object();
+        private bool _value;
+        public ThreadSafeBool(bool initialValue)
+        {
+            _value = initialValue;
+        }
+        public bool Value
+        {
+            get
+            {
+                lock (_locker)
+                {
+                    return _value;
+                }
+            }
+            set
+            {
+                lock (_locker)
+                {
+                    _value = value;
+                }
+            }
+        }
+    }
+    public interface ITaskRequest
+    {
+
+    }
+    public interface IQueueableTaskExecutor<TaskRequestType> : ITaskRequest where TaskRequestType : class, ITaskRequest
+    {
+        void Initialize();
+        void Execute(TaskRequestType request);
+    }
+    public class TaskQueue<QueueableTaskExecutorType, TaskRequestType>
+        where QueueableTaskExecutorType : class, IQueueableTaskExecutor<TaskRequestType>, new()
+        where TaskRequestType : class, ITaskRequest
+    {
+        private QueueableTaskExecutorType _execotor;
+        private object _locker = new object();
+        private ThreadSafeBool _isProcessingQueue;
+        private ThreadSafeBool _isStopped;
+        private ConcurrentQueue<TaskRequestType> concurrentQueue = new ConcurrentQueue<TaskRequestType>();
+
+        /// <summary>
+        /// Private constructor
+        /// </summary>
+        private TaskQueue()
+        {
+
+        }
+
+        private static Lazy<TaskQueue<QueueableTaskExecutorType, TaskRequestType>> _lazy = new Lazy<TaskQueue<QueueableTaskExecutorType, TaskRequestType>>(() =>
+        {
+            var te = new TaskQueue<QueueableTaskExecutorType, TaskRequestType>();
+            te.Initialize();
+            return te;
+        });
+
+        private void Initialize()
+        {
+            _execotor = new QueueableTaskExecutorType();
+            _execotor.Initialize();
+            _isProcessingQueue = new ThreadSafeBool(false);
+            _isStopped = new ThreadSafeBool(false);
+        }
+
+        private void ProcessQueueItems()
+        {
+            TaskRequestType request;
+            while (!_isStopped.Value && concurrentQueue.TryDequeue(out request))
+            {
+                _execotor.Execute(request);
+            }
+            _isProcessingQueue.Value = false;
+        }
+        /// <summary>
+        /// If no worker is processing the queue then initiate a worker to finish the pending items.
+        /// </summary>
+        private void ExecutePendingQueueItems()
+        {
+            if (!_isProcessingQueue.Value)
+            {
+                _isProcessingQueue.Value = true;
+                Task.Run(() => ProcessQueueItems());
+            }
+        }
+        public static TaskQueue<QueueableTaskExecutorType, TaskRequestType> CreateInstance()
+        {
+            return _lazy.Value;
+        }
+
+        public bool Enqueue(TaskRequestType request)
+        {
+            lock (_locker)
+            {
+                var result = false;
+                //When stopped, do not accept new items.
+                if (!_isStopped.Value)
+                {
+                    result = true;
+                    concurrentQueue.Enqueue(request);
+                    ExecutePendingQueueItems();
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Stop processing the pending queue items.
+        /// </summary>
+        public void Stop()
+        {
+            lock (_locker)
+            {
+                _isStopped.Value = true;
+            }
+        }
+
+        /// <summary>
+        /// Start processing pending queue items.
+        /// </summary>
+        public void Start()
+        {
+            lock (_locker)
+            {
+                _isStopped.Value = false;
+                if (!concurrentQueue.IsEmpty)
+                    ExecutePendingQueueItems();
+            }
+        }
+    }
+}
+
